248e133d296898d44cd60f7599e17348b6916540
overlookmotel <theoverlookmotel@gmail.com>
2020-04-04 12:51:22 +0100

    Code comments [nocode]

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import {isFunction} from 'is-it-type';
 
 // Exports
 
+/**
+ * Determine if input is a React class component.
+ * Input must have already been checked that it's a function before calling this.
+ * @param {Function} Component - Function
+ * @return {boolean} - true if is a React class component
+ */
 export default function isClassComponent(input) {
 	return isFunction(input) && !!(input.prototype && input.prototype.isReactComponent);
 }
